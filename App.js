e10c131197212a32893f9402cbdfbb45e5af528b
divyanshchoudhary2019 <58388053+divyanshchoudhary2019@users.noreply.github.com>
2020-08-21 15:56:37 +0530

    Update App.js

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: 'blue',
   },
   inputBox: {
     marginTop: 50,
@@ -95,6 +95,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 10,
     margin: 10,
+    boderRadius:21,
   },
   buttonText: {
     textAlign: 'center',
