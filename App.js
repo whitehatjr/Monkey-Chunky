3fdcf787e31acc1df31113848a66cf1ad52c52f2
KartikeyRaghav <abhayjeetraghav@gmail.com>
2020-10-09 20:52:06 -0400

    Modifying App.js

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: '#b9b9b9',
   },
   inputBox: {
     marginTop: 50,
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
+    borderRadius:20
   },
   goButton: {
     width: '50%',
