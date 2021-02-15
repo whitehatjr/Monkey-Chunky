acb680e68db951ccfc64645d261407aaaf7c2b2b
RS0509 <riddhimasarin@gmail.com>
2021-02-14 20:51:24 -0500

    changed color

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header
-          backgroundColor={'#9c8210'}
+          backgroundColor={'yellow'}
           centerComponent={{
             text: 'Monkey Chunky',
             style: { color: '#fff', fontSize: 20 },
