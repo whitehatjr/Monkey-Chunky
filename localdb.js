3675d5c0165dd09dd9dbd0a313ed14d002b056a4
ReevaPrince <loboprince740@gmail.com>
2021-03-09 17:31:14 +0530

    round corner fo input box

--- localdb.js
+++ localdb.js
@@ -30,5 +30,6 @@ const db = {
   an: { chunks: ['a', 'n'], phones: ['AE', 'N'] },
   was: { chunks: ['w', 'a', 's'], phones: ['W', 'AA', 'Z'] },
   we: { chunks: ['w', 'e'], phones: ['W', 'IY'] },
+  
 };
 export default db;
