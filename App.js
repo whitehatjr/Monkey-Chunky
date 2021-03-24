fa05367879880169c4424aad3b0b9b00d065f263
AnyaKunder <69795755+AnyaKunder@users.noreply.github.com>
2021-03-24 17:21:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -25,10 +25,10 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header
-          backgroundColor={'#9c8210'}
+          backgroundColor={'#9c8221'}
           centerComponent={{
             text: 'Monkey Chunky',
-            style: { color: '#fff', fontSize: 20 },
+            style: { color: '#fff', fontSize: 22 },
           }}
         />
 
@@ -57,7 +57,7 @@ export default class App extends React.Component {
             ):
             Alert.alert("The word does not exist in our database");
           }}>
-          <Text style={styles.buttonText}>GO</Text>
+          <Text style={styles.buttonText}>SUBMIT</Text>
         </TouchableOpacity>
         <View>
           {this.state.chunks.map((item, index) => {
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#b8b8b8',
   },
   inputBox: {
-    marginTop: 50,
+    marginTop: 70,
     width: '80%',
     alignSelf: 'center',
     height: 40,
