d8aa2ba7ff42dc97675b12975bc6e73c7e67d19f
lakshay123-ctrl <70754884+lakshay123-ctrl@users.noreply.github.com>
2021-03-24 16:48:11 +0530

    border width changed

--- App.js
+++ App.js
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     height: 40,
     textAlign: 'center',
-    borderWidth: 4,
+    borderWidth: 10,
     outline: 'none',
   },
   goButton: {
