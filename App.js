76e69315ad973a4e10d0088d2c95086b0062fade
patelparva <patelparva6218@gmail.com>
2021-03-27 19:18:55 +0530

    App

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends React.Component {
         <Header
           backgroundColor={'#9c8210'}
           centerComponent={{
-            text: 'Monkey Chunky',
+            text: 'Monkey Chunky App',
             style: { color: '#fff', fontSize: 20 },
           }}
         />
