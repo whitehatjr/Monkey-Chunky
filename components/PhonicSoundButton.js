eb1d7dda3aa749f4f0367ac4d8cf177cdd4df4e3
DeepaRaj123 <36413136+DeepaRaj123@users.noreply.github.com>
2021-03-31 17:25:57 +0530

    Update PhonicSoundButton.js

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -37,7 +37,7 @@ export default class PhonicSoundButton extends React.Component {
         }}>
         <Text style={
           this.props.buttonIndex === this.state.pressButtonIndex
-                        ? [styles.displayText, { color: 'red' }]
+                        ? [styles.displayText, { color: 'blue' }]
                         : [styles.displayText, { color: 'white' }]
         }>{this.props.wordChunk}</Text>
       </TouchableOpacity>
