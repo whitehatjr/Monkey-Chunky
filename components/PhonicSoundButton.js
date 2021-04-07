f9451692d2c264672f5e47b3fcf825a03dda31f5
DeepaD-gith <68142989+DeepaD-gith@users.noreply.github.com>
2021-04-07 18:22:49 +0530

    Update PhonicSoundButton.js

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -55,6 +55,7 @@ const styles = StyleSheet.create({
     width: '60%',
     height: 50,
     justifyContent: 'center',
+    flexDirection: 'horizontal',
     alignItems: 'center',
     alignSelf: 'center',
     borderRadius: 10,
