5d62f0a5f28d85e30c9ff7bfab1c18d64e1e0f76
Agastya-Bhise <agastya.bhise@gmail.com>
2021-05-07 17:27:32 +0530

    Made some quality of life changes (style properties)

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header
-          backgroundColor={'#9c8210'}
+          backgroundColor={'red'}
           centerComponent={{
             text: 'Monkey Chunky',
             style: { color: '#fff', fontSize: 20 },
@@ -87,6 +87,7 @@ const styles = StyleSheet.create({
     height: 40,
     textAlign: 'center',
     borderWidth: 4,
+    borderRadius: 20,
     outline: 'none',
   },
   goButton: {
