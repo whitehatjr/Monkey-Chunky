3bdf5679daae12961546b00a061f46de89a15d83
PranavKandakurthi <kandakurthipranav@gmail.com>
2021-05-13 20:48:44 -0400

    Colors have been changed

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -28,8 +28,8 @@ export default class PhonicSoundButton extends React.Component {
       <TouchableOpacity
         style={
           this.props.buttonIndex === this.state.pressButtonIndex
-                      ? [styles.chunkButton, { backgroundColor: 'white' }]
-                      : [styles.chunkButton, { backgroundColor: 'red' }]
+                      ? [styles.chunkButton, { backgroundColor: 'black' }]
+                      : [styles.chunkButton, { backgroundColor: 'skyblue' }]
           }
         onPress={() => {
           this.setState({ pressButtonIndex: this.props.buttonIndex });
@@ -37,8 +37,8 @@ export default class PhonicSoundButton extends React.Component {
         }}>
         <Text style={
           this.props.buttonIndex === this.state.pressButtonIndex
-                        ? [styles.displayText, { color: 'red' }]
-                        : [styles.displayText, { color: 'white' }]
+                        ? [styles.displayText, { color: 'skyblue' }]
+                        : [styles.displayText, { color: 'black' }]
         }>{this.props.wordChunk}</Text>
       </TouchableOpacity>
     );
