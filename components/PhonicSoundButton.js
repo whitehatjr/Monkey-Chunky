5241255344733d649403834e99dbfb10e2b6fc2d
PranavKandakurthi <kandakurthipranav@gmail.com>
2021-05-13 20:42:33 -0400

    Changed flex direction.

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -58,6 +58,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
     borderRadius: 10,
+    flexDirection: "row",
     margin: 5,
     backgroundColor: 'red'
   }
