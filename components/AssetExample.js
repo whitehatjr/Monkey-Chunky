0f4dd70a3a86d29270e2bd3fc6d3bc9a2f4462ad
YatinSai <nshadow873@gmail.com>
2021-05-21 20:31:14 -0400

    Added a Touchable Opacity

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, ToucableOpacity, TouchableOpacity } from 'react-native';
 
 export default class AssetExample extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Local files and assets can be imported by dragging and dropping them into the editor
-        </Text>
+        <TouchableOpacity><Text>MadeAnUpdate</Text></TouchableOpacity>
         <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
       </View>
     );
