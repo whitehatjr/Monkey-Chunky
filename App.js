702bfdd8e9466a14cd6d1b894fa52b62deaa07b2
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2021-05-25 17:54:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+import PhonicSoundButton from './components/PhonicSoundButton';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+      phonicSounds: [],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'#9c8210'}
+          centerComponent={{
+            text: 'Monkey Chunky',
+            style: { color: 'grey', fontSize: 21, fontWeight: 'bold', },
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            var word = this.state.text.toLowerCase().trim();
+            db[word]?(
+            this.setState({ chunks: db[word].chunks }),
+            this.setState({ phonicSounds: db[word].phones })
+            ):
+            Alert.alert("The word does not exist in our database");
+          }}>
+          <Text style={styles.buttonText}>GO</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item, index) => {
+            return (
+              <PhonicSoundButton
+                wordChunk={this.state.chunks[index]}
+                soundChunk={this.state.phonicSounds[index]}
+                buttonIndex={index}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 2,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Monkey Chunky App
+
+Monkey Chunky App is designed to help early stage readers practice reading words. The user can enter any word and the app will break it down into phonic chunks. User can tap on the phonic chunks to hear the corresponding sound of the phones that make the word.
