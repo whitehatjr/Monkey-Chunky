6fb49a2f9e974b5fff3f4b7e6283e19536ed5553
Sowmya <sowmya.pn@gmail.com>
2021-05-29 17:10:43 +0530

    changed radius

--- App.js
+++ App.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderWidth: 4,
     outline: "none",
-    borderRadius: 20,
+    borderRadius: 50,
   },
   goButton: {
     width: "50%",
