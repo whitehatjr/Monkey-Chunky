2f358a95c11db073aeba15416ea52af02316a726
Mac <divs20.2004@gmail.com>
2021-06-15 20:43:42 +0530

    changed Alert to alert

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default class App extends React.Component {
             this.setState({ chunks: db[word].chunks }),
             this.setState({ phonicSounds: db[word].phones })
             ):
-            Alert.alert("The word does not exist in our database");
+            alert("The word does not exist in our database");
           }}>
           <Text style={styles.buttonText}>GO</Text>
         </TouchableOpacity>
