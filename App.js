9822f71c949df2f8a9af57134a0437ce278675f0
Akhil <akhilrahamath@gmail.com>
2021-07-15 21:41:44 +0530

    changed color

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: 'light pink',
   },
   inputBox: {
     marginTop: 50,
