6114e6e03c77a58b4afb6d5c9e996fabe6737947
saanvi13 <saanvikaistha7@gmail.com>
2021-08-03 15:03:38 +0530

    input box rounded

--- App.js
+++ App.js
@@ -59,7 +59,8 @@ export default class App extends React.Component {
           }}>
           <Text style={styles.buttonText}>GO</Text>
         </TouchableOpacity>
-        <View >
+        <View style = {{flexDirection: "row",alignSelf:"center"}}>
+
           {this.state.chunks.map((item, index) => {
             return (
               <PhonicSoundButton
@@ -88,7 +89,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius: 25,
+    borderRadius: 20,
   },
   goButton: {
     width: '50%',
