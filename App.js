71a676982ffee54bc56b6df528b8472f2300519b
sritika780@gmail.com <ritikasharma9695@gmail.com>
2021-09-02 14:07:39 +0530

    changed bg color to pink

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'brown',
+    backgroundColor: 'pink',
   },
   inputBox: {
     marginTop: 50,
