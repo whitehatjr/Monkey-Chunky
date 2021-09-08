f594b12b94ad80d1225777791c6ef2fe51d948f2
tanishkapunde <tanishkapunde09@gmail.com>
2021-09-08 15:40:10 +0530

    round corner and flex

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ export default class App extends React.Component {
           }}>
           <Text style={styles.buttonText}>GO</Text>
         </TouchableOpacity>
-        <View>
+        <View style={{flexDirection:"row", alignSelf:"center"}}>
           {this.state.chunks.map((item, index) => {
             return (
               <PhonicSoundButton
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
+    borderRadius: 20,
   },
   goButton: {
     width: '50%',
