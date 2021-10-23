5801e1e37afe93d21e6348736fc1c8f8810755ed
hemavathi-github <75414397+hemavathi-github@users.noreply.github.com>
2021-10-23 19:25:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header
-          backgroundColor={'#9c8210'}
+          backgroundColor={'#9c8200'}
           centerComponent={{
             text: 'Monkey Chunky',
             style: { color: '#fff', fontSize: 40 },
