38db0380933cee26f87347796705039473a9486c
Rithvik157 <jai018@gmail.com>
2021-11-07 12:42:09 +0530

    change styling

--- App.js
+++ App.js
@@ -81,10 +81,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#b8b8b8',
   },
   inputBox: {
-    marginTop: 50,
+    marginTop: 70,
     width: '80%',
     alignSelf: 'center',
-    height: 40,
+    height: 65,
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
