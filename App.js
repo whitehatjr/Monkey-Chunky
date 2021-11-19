d50e7fadec690a807dfa817e37f05d2ed158df45
Teja2008 <tejarajaramesh999@gmail.com>
2021-11-19 17:37:22 +0530

    App.js updates with new colors and gifs.

--- App.js
+++ App.js
@@ -1,75 +1,83 @@
 import * as React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  Alert
-} from 'react-native';
-import { Header } from 'react-native-elements';
-import db from './localdb';
-import PhonicSoundButton from './components/PhonicSoundButton';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { Appbar, Avatar, TextInput } from 'react-native-paper';
+import data from './localdb';
+import {Audio} from 'expo-av';
+import * as Speech from 'expo-speech';
+
+// console.log(data['one'].phones[2])
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      text: '',
+      word: '',
       chunks: [],
-      phonicSounds: [],
+      phones: [],
     };
   }
+
+  playSound=(x)=>{
+    Audio.Sound.createAsync({uri:"https://s3-whitehatjrcontent.whjr.online/phones/"+x+".mp3"},{shouldPlay:true})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header
-          backgroundColor={'#9c8210'}
-          centerComponent={{
-            text: 'Monkey Chunky',
-            style: { color: '#fff', fontSize: 20 },
-          }}
-        />
+        <Appbar.Header style={{ backgroundColor: '#9F2C55' }}>
+          <Appbar.Content
+            title="Monkey Chunky App"
+            subtitle="Helps students read better!"
+          />
+        </Appbar.Header>
 
-        <Image
-          style={styles.imageIcon}
+        <Avatar.Image
+          style={styles.image}
+          size={150}
           source={{
             uri:
-              'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
+              'https://i.pinimg.com/originals/33/d1/ca/33d1ca54700482f2c4ef8483219f0bc8.gif',
           }}
         />
 
         <TextInput
-          style={styles.inputBox}
-          onChangeText={text => {
-            this.setState({ text: text });
+          label={'word'}
+          onChangeText={(x) => {
+            this.setState({ word: x });
           }}
-          value={this.state.text}
+          placeHolder={'Enter a word!'}
+          style={{ margin: 20 }}
+          theme={{ colors: { primary: '#9F2C55', underlineColor: 'red' } }}
         />
-        <TouchableOpacity
-          style={styles.goButton}
+
+        <Button
           onPress={() => {
-            var word = this.state.text.toLowerCase().trim();
-            db[word]?(
-            this.setState({ chunks: db[word].chunks }),
-            this.setState({ phonicSounds: db[word].phones })
-            ):
-            Alert.alert("The word does not exist in our database");
-          }}>
-          <Text style={styles.buttonText}>GO</Text>
-        </TouchableOpacity>
-        <View>
-          {this.state.chunks.map((item, index) => {
-            return (
-              <PhonicSoundButton
-                wordChunk={this.state.chunks[index]}
-                soundChunk={this.state.phonicSounds[index]}
-                buttonIndex={index}
-              />
-            );
-          })}
-        </View>
+            var word = this.state.word.toLowerCase().trim();
+            Speech.speak(word);
+            if(data[word]){
+            this.setState({ chunks: data[word].chunks });
+            this.setState({ phones: data[word].phones });
+            }
+            else{
+              alert("The word you have entered was not found in our database, please try another one.")
+            }
+          }}
+          title="Click for chunks"
+          color="#9F2C55"
+        />
+
+        {this.state.chunks.map((x,i) => {
+          return(
+            <TouchableOpacity onPress={()=>{this.playSound(this.state.phones[i])}}>
+          <Text style={styles.text}>
+          {x}
+          </Text>
+           </TouchableOpacity>
+          )
+        })}
+
+
+
       </View>
     );
   }
@@ -78,32 +86,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: 'coral',
   },
-  inputBox: {
-    marginTop: 50,
-    width: '80%',
+  image: {
     alignSelf: 'center',
-    height: 40,
-    textAlign: 'center',
-    borderWidth: 4,
-    outline: 'none',
+    margin: 25,
   },
-  goButton: {
-    width: '50%',
-    height: 55,
-    alignSelf: 'center',
-    padding: 10,
-    margin: 10,
-  },
-  buttonText: {
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  imageIcon: {
-    width: 150,
-    height: 150,
-    marginLeft: 95,
+
+  text:{
+    alignSelf:'center',
+    padding: 2,
+    margin: 7,
+    backgroundColor: 'white'
   }
 });
