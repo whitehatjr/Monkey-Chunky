70486a3fd436e9548e7c77f3e562722dc2abe08d
manasvani <73254884+manasvani@users.noreply.github.com>
2021-11-19 16:08:42 +0530

    Update PhonicSoundButton.js

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -48,7 +48,7 @@ export default class PhonicSoundButton extends React.Component {
 const styles = StyleSheet.create({
   displayText: {
     textAlign: 'center',
-    fontSize: 40,
+    fontSize: 50,
     color: 'white'
   },
   chunkButton:{
