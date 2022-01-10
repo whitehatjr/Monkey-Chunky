930123ce927efea111cb633a0f8c65d5d9630677
Sayoon-K <84568721+Sayoon-K@users.noreply.github.com>
2022-01-10 13:30:56 +0530

    secondtchange

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
             style: { color: '#fff', fontSize: 30 },
           }}
         />
-
+<View style={{flexDirection:"row"}} >
         <Image
           style={styles.imageIcon}
           source={{
@@ -39,7 +39,14 @@ export default class App extends React.Component {
               'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
           }}
         />
-
+        <Image
+          style={styles.imageIcon}
+          source={{
+            uri:
+              'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
+          }}
+        />
+</View>
         <TextInput
           style={styles.inputBox}
           onChangeText={text => {
