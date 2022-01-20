5b108c1a4ecaf37a778f1bbf97ae93c7ecfdecc7
raghav517 <raghav.gajpure@gmail.com>
2022-01-20 17:06:22 +0530

    changed background

--- components/PhonicSoundButton.js
+++ components/PhonicSoundButton.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    borderRadius: 10,
+    borderRadius: 100,
     margin: 5,
     backgroundColor: 'red'
   }
