3b3b649df2ff886ec894524c471927ed8bb6d38e
krrish012 <69247472+krrish012@users.noreply.github.com>
2022-01-29 11:57:16 +0530

    Updated

--- App.js
+++ App.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
-    borderRadius:0,
+    borderRadius:40,
   },
   goButton: {
     width: '50%',
