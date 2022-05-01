56dcde3a437380fa734af32f01df95d36ce23e76
Sparshie123 <sparshmadan7@gmail.com>
2022-05-01 10:18:57 +0100

    I added to round the corners for the inputBox and added a colour for the border

--- App.js
+++ App.js
@@ -88,6 +88,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
+    borderRadius:20,
+    borderColor:"lime"
   },
   goButton: {
     width: '50%',
