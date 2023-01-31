129aeb188ef7ffd4c6fd9151167ed9bb43002441
vaishnavi <msva0708@gmail.com>
2023-01-31 17:50:23 +0530

    changed the magin and the size of the input box

--- App.js
+++ App.js
@@ -81,10 +81,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#b8b8b8',
   },
   inputBox: {
-    marginTop: 50,
+    marginTop: 20,
     width: '80%',
     alignSelf: 'center',
-    height: 40,
+    height: 50,
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
