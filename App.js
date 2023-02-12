90a07c135d7fd2d92fa8c743be3bd682f338febb
putinea <mrrhys2017@gmail.com>
2023-02-12 16:16:55 +0800

    updated background colour

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: 'grey',
   },
   inputBox: {
     marginTop: 50,
