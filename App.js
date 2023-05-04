3eca1a8decdcafa34dd3683a136d9ab3acd21d9d
Ammoditaa123 <himyemailid2021@gmail.com>
2023-05-04 20:57:12 +0530

    Background color has been changed and border radius has been added

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: '#E4CCEB',
   },
   inputBox: {
     marginTop: 50,
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     borderWidth: 4,
     outline: 'none',
+    borderRadius: 3,
   },
   goButton: {
     width: '50%',
