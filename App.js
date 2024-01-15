889668a12d807b0e224349207fd61802b7c4e842
Samuel Aware <75440151+Samuel-4525@users.noreply.github.com>
2024-01-15 16:52:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8b8b8',
+    backgroundColor: '#b9b9b9',
   },
   inputBox: {
     marginTop: 50,
